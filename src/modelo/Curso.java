2775c489405afd66d25f322c8fd8efdb23c91714
Andres Mauricio Velasquez Montoya <amvelasquez@personalsoft.com.co>
2019-12-26 14:26:25 -0500

    se sube el archivo de curso con el codigo

--- src/modelo/Curso.java
+++ src/modelo/Curso.java
@@ -2,4 +2,44 @@ package modelo;
 
 public class Curso {
 
+    private String codigoCurso;
+    private String nombreCurso;
+    private int creditos;
+    private double nota;
+
+
+    public Curso (String cod, String nom, int cred){
+        this.nombreCurso = nom;
+        this.codigoCurso = cod;
+        this.creditos = cred;
+    }
+
+    public int getCreditos(){
+        return creditos;
+    }
+
+    public void setCreditos(int nuevosCreditos){
+        this.creditos = nuevosCreditos;
+    }
+
+    public double getNota(){
+        return nota;
+    }
+
+    public void setNota(double nota){
+        this.nota = nota;
+    }
+
+    public String getNombreCurso(){
+        return nombreCurso;
+    }
+
+    public String getDescripcion(){
+        return "Retorna descripcion";
+    }
+
+    public boolean estaCalificado(){
+        return false;
+    }
+
 }
